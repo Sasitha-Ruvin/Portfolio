3c44409b142af933d28a8b1ed22e80b492ccd5ad
Sasith-Ruvin <cb011152@students.apiit.lk>
2024-10-20 11:05:49 +0530

    minor changes

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -15,7 +15,7 @@ const Clients = () => {
               direction='right'
               speed='slow' 
             />
-        <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10'>
+        {/* <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10'>
           {companies.map(({id, img, name, nameImg})=>(
             <div key={id} className='flex md:max-w-60 max-w-32 gap-32'>
               <img src={img} alt={name} className='md:w-10 w-5' />
@@ -23,7 +23,7 @@ const Clients = () => {
 
             </div>
           ))}
-        </div>  
+        </div>   */}
       </div>
     </div>
   )
